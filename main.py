08267542f649c218119735167526137a235d4037
Deepika Shrestha <sthadpka93@gmail.com>
2021-07-01 14:43:03 +0000

    Replace main.py

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ import pyspark
 spark = pyspark.sql.SparkSession \
     .builder \
     .appName("Airflow") \
-    .config('spark.driver.extraClassPath', "/home/deepika/Documents/postgresql-42.2.22.jar") \
+    .config('spark.driver.extraClassPath', "/home/deepika/Documents/driver_jar_file") \
     .getOrCreate()
 
 
 def load_to_db(df):
     mode="overwrite"
-    url="jdbc:postgresql://localhost:5432/mytest"
-    properties={"user":"postgres",
-                "password":"qw3rty123",
+    url="jdbc:postgresql://localhost:5432/databasename"
+    properties={"user":<username>,
+                "password":<password>,
                 "driver":"org.postgresql.Driver"
         
     }
