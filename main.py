4bd571cf0fb05ce642c509c8378e2e0ed5d1d250
Deepika Shrestha <sthadpka93@gmail.com>
2021-07-01 12:43:49 +0000

    Upload New File

---
+++ main.py
@@ -0,0 +1,24 @@
+import pyspark
+from Project import *
+
+spark = pyspark.sql.SparkSession \
+    .builder \
+    .appName("Airflow") \
+    .config('spark.driver.extraClassPath', "/home/deepika/Documents/postgresql-42.2.22.jar") \
+    .getOrCreate()
+
+
+def load_to_db(df):
+    mode="overwrite"
+    url="jdbc:postgresql://localhost:5432/mytest"
+    properties={"user":"postgres",
+                "password":"qw3rty123",
+                "driver":"org.postgresql.Driver"
+        
+    }
+    df.write.jdbc(url=url,
+                  table="Project",
+                  mode=mode,
+                 properties= properties)
+
+
